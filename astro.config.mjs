849fd02866a69084db95b3fb0ea48fe62b02ce62
Ayaskant Mishra <ayas.optimark@gmail.com>
2024-03-25 19:49:45 +0530

    provisioned astro db and tinyMCE

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aysastresume.vercel.app',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), db()]
 });
