148e5f511ae0f8f473835d550dcc4e137feeccbb
Ayaskant Mishra <ayas.optimark@gmail.com>
2024-05-22 13:15:58 +0530

    [dirty] Pages editor base commit
    - Added vite yaml plugin and provisioned astro vite dependency with it.
    - Added basic home.yml  to content folder to for homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import yaml from '@rollup/plugin-yaml';
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aysastresume.vercel.app',
+  vite: {
+    plugins: [yaml()],
+  },
   integrations: [mdx(), sitemap(), tailwind(), react(), db()]
 });
