b1c8d9e5604e6b4df2f4525f6dac78c33e0959ed
Ayaskant Mishra <ayas.optimark@gmail.com>
2024-06-01 19:49:38 +0530

    Dynamic editor configuration
    - Build script added to inject current branch name into editor
    - added environment utils to provision environment related changes
    - modified content-manager template to use new utils instead of directly accessing nodenv

--- src/components/Decap.tsx
+++ src/components/Decap.tsx
@@ -2,20 +2,22 @@ import React, { useEffect } from 'react'
 import CMS from "decap-cms-app";
 import netlifyIdentity from "netlify-identity-widget";
 import DecapConfig from "../configs/editor.yml";
-import type { CmsConfig } from "decap-cms-core"; 
+import type { CmsConfig } from "decap-cms-core";
+import { getEditorBranch } from '../utils/environment';
 
 const EditorConfig: CmsConfig = DecapConfig as CmsConfig;
+EditorConfig.backend.branch = getEditorBranch();
 
 const Decap = () => {
 
   netlifyIdentity.on("init", (user: netlifyIdentity.User | null) => {
     if (!user) {
       netlifyIdentity.open("login");
-    }else{
+    } else {
       (window as any).netlifyIdentity = netlifyIdentity;
-      CMS.init({config: EditorConfig});
+      CMS.init({ config: EditorConfig });
     }
-    
+
   });
 
   useEffect(() => {
